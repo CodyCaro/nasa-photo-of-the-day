1fc6a2bb4123326bec201ab3ec230cad51a640e1
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-07-30 18:55:46 -0700

    Added a button to toggle the content and stlyised the card to look decent

--- src/ApodCard.js
+++ src/ApodCard.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ApodCard.scss";
 
 function ApodCard(props) {
   // console.log(props.url);
+  const [wantingToReadMore, setWantingToReadMore] = useState(false);
+  function handleReadMoreClick(e) {
+    e.preventDefault();
+    setWantingToReadMore(!wantingToReadMore);
+  }
   return (
     <div className="card">
       <h2>{props.title}</h2>
+      <h3>
+        {props.copyright} ({props.date})
+      </h3>
       <img src={props.url} alt={props.explanation} />
-      <h3>{props.copyright}</h3>
-      <h3>{props.date}</h3>
-      <h3>{props.explanation}</h3>
+      <h3 className="date" />
+      <button onClick={handleReadMoreClick}>Read More</button>
+      <h3 className={wantingToReadMore ? "read-more" : "remove"}>
+        {props.explanation}
+      </h3>
     </div>
   );
 }
